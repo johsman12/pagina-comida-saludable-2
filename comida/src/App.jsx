9b4bf849167323a234a283a742efcfba0c2a557b
juliana <eilynjb08@gmail.com>
2026-04-21 08:34:10 -0500

    Limpieza manual de conflictos y combinación de componentes

--- comida/src/App.jsx
+++ comida/src/App.jsx
@@ -1,34 +1,18 @@
 import Header from "./componentes/Header";
 import Botones from "./componentes/Botones";
-<<<<<<< HEAD
-import Home from "./componentes/Home";
-import Productos from "./componentes/Productos";
-import Footer from "./componentes/Footer";
-=======
 import Productos from "./componentes/Productos";
 import Footer from "./componentes/Footer";
 import Login from './componentes/Login';
->>>>>>> juliana
 import "./App.css";
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Header />
-      <Botones />
-      <Home />
-=======
-      {/* Aquí combinamos: 
-          Mantenemos los componentes de Master activos 
-          y dejamos tu Login también.
-      */}
       <Header />
       <Botones />
       
       <Login /> 
       
->>>>>>> juliana
       <Productos />
       <Footer />
     </>
